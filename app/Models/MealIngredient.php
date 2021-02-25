85c9e1f71680c3619c849e1f351336cd68ab45b9
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-02-25 14:48:17 +0000

    Update env file

---
+++ app/Models/MealIngredient.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MealIngredient extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'meal';
+
+    protected $fillable = [
+        'meal_id',
+        'ingredient_id'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'meal_id' => 'integer',
+        'ingredient_id' => 'integer',
+    ];
+
+    public function meal(): BelongsTo
+    {
+        return $this->belongsTo(Meal::class, 'meal_id', 'id');
+    }
+
+    public function ingredient(): BelongsTo
+    {
+        return $this->belongsTo(Ingredient::class, 'ingredient_id', 'id');
+    }
+}
