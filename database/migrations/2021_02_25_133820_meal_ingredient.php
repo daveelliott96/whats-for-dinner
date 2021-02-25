bcf3f00eed14879f7c37ba1d5a82a601ca03a673
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-02-25 14:51:01 +0000

    Add meal_ingredient migration file

---
+++ database/migrations/2021_02_25_133820_meal_ingredient.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MealIngredient extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal_ingredient', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->integer('meal_id');
+            $table->integer('ingredient_id');
+            $table->string('name');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('deleted_at')->nullable();
+
+            $table->foreign('meal_id')->references('id')->on('meal');
+            $table->foreign('ingredient_id')->references('id')->on('ingredient');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal_ingredient');
+    }
+}
