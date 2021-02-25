c3c0321aa704a47362a93378ea7b501e56263aa5
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-02-25 14:51:54 +0000

    Add Meal model

---
+++ app/Models/Meal.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Meal extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'meal';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+    ];
+
+    public function mealIngredient(): HasMany
+    {
+        return $this->hasMany(MealIngredient::class, 'meal_id', 'id');
+    }
+}
