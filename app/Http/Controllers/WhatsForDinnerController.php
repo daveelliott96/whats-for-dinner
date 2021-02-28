69ec04418a28ee17c5a90eedbb6a6de7a7f6516c
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-02-28 20:51:08 +0000

    Add get route and failing test

--- app/Http/Controllers/WhatsForDinnerController.php
+++ app/Http/Controllers/WhatsForDinnerController.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+class WhatsForDinnerController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'data' => '',
+        ])->setStatusCode(200);
+    }
+}
--- tests/Feature/GetRouteTest.php
+++ tests/Feature/GetRouteTest.php
@@ -1,21 +1,86 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Ingredient;
+use App\Models\Meal;
+use App\Models\MealIngredient;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class GetRouteTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    private $meals;
+    private $ingredients;
+    private $mealIngredients;
+
+    private const API_URL = '/api/meals';
+
+    public function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->meals = Meal::all();
+        $this->ingredients = Ingredient::all();
+        $this->mealIngredients = MealIngredient::all();
+
+        $this->deleteTableData();
+
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->deleteTableData();
+
+        $this->meals->save();
+        $this->ingredients->save();
+        $this->mealIngredients->save();
+    }
+
+    public function testItGetsMealData(): void
+    {
+        Meal::create(
+            ['name' => 'meal1'],
+        );
+
+        $expected = [
+            'data' => [
+                0 => [
+                    'meal_id' => 1,
+                    'meal_name' => 'meal1',
+                    'ingredients' => [
+                        0 => [
+                            'ingredient_id' => 1,
+                            'name' => 'Chicken',
+                        ],
+                        1 => [
+                            'ingredient_id' => 2,
+                            'name' => 'Ham',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $response = $this->get(self::API_URL);
 
         $response->assertStatus(200);
+        $response->assertExactJson($expected);
+    }
+
+    private function deleteTableData(): void
+    {
+        foreach (MealIngredient::all() as $mealIngredient) {
+            $mealIngredient->forceDelete();
+        }
+
+        foreach (Meal::all() as $meal) {
+            $meal->forceDelete();
+        }
+
+        foreach (Ingredient::all() as $ingredient) {
+            $ingredient->forceDelete();
+        }
     }
 }
