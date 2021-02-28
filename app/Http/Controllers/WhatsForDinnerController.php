fec1afc953bbd3f90ce47a1832d369b7964b345c
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-02-28 20:53:46 +0000

    WIP Add some logic to get route

--- app/Http/Controllers/WhatsForDinnerController.php
+++ app/Http/Controllers/WhatsForDinnerController.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Meal;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class WhatsForDinnerController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+        $meals = Meal::with('mealIngredient')->get();
+
         return response()->json([
-            'data' => '',
+            'data' => $meals,
         ])->setStatusCode(200);
     }
 }
