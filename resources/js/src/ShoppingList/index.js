3a6035fb6604fc996901721e5c5ee110a7f7c30f
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-04-23 13:12:19 +0100

    WIP display shopping list and copy to clipboard

--- resources/js/src/ShoppingList/index.js
+++ resources/js/src/ShoppingList/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavigationButton } from "../components/navigation-button"
 import styled from "styled-components"
 import { ExportButton } from "../MealList/components/Buttons/components/export-button"
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 export const ButtonsContainer = styled.div`
   display:flex;
@@ -10,13 +11,55 @@ export const ButtonsContainer = styled.div`
 `
 
 function ShoppingList({ selectedMeals }) {
+  const [shoppingList, setShoppingList] = useState('')
+  const [copied, setCopied] = useState(false)
+
+  useEffect(() => {
+    generateShoppingList()
+  }, [])
+
+  const generateShoppingList = () => {
+    let ingredientsForShoppingList = []
+    selectedMeals.forEach(meal => {
+      meal.ingredients.forEach(ingredient => {
+        ingredientsForShoppingList.push(ingredient.ingredient_name)
+      })
+    })
+
+    let uniqueIngredients = [...new Set(ingredientsForShoppingList)]
+
+     uniqueIngredients.join(' ')
+
+    setShoppingList(uniqueIngredients)
+  }
+
   return (
     <>
       <ButtonsContainer>
         <NavigationButton children={'Back to meal list'} linkTo={'/'}/>
-        <ExportButton children={'Copy shopping list to clipboard'}/>
+        <CopyToClipboard text={shoppingList} onCopy={() => setCopied(true)}>
+          <ExportButton children={'Copy shopping list to clipboard'}/>
+        </CopyToClipboard>
       </ButtonsContainer>
-      <p>You've added {selectedMeals.length} meals to your shopping list:</p>
+      <p>You've added {selectedMeals.length} {selectedMeals.length === 1 ? 'meal' : 'meals'} to your shopping list:</p>
+      {
+        selectedMeals.map(meal => {
+          return (
+            <>
+              <p>{meal.meal_name}</p>
+              <ul>
+                {
+                  meal.ingredients.map(ingredient => {
+                    return (
+                      <li>{ingredient.ingredient_name}</li>
+                    )
+                  })
+                }
+              </ul>
+            </>
+          )
+        })
+      }
     </>
   )
 }
