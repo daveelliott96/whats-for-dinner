fe3538ae4afae7d8b4d837c883376842057306b4
Dave Elliott <dave_elliott96@hotmail.co.uk>
2021-07-13 15:04:34 +0100

    Bit of refactor

--- resources/js/src/components/PageHeader/index.js
+++ resources/js/src/components/PageHeader/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { PageHeaderContainer } from "./components/page-header-container"
-import { PageTitle } from "./components/page-title"
+import styled from "styled-components"
+
+const PageHeaderContainer = styled.div`
+  text-align: center;
+  background-color: #24265A;
+  width: 100%;
+  padding-top: 16px;
+  padding-bottom: 16px;
+
+  @media only screen and (max-width: 992px) {
+    padding-top: 8px;
+    padding-bottom: 8px;
+  }
+`
+
+const StyledPageTitle = styled.h1`
+  color: white;
+  font-weight: lighter;
+  font-size: 2.5rem;
+
+  @media only screen and (max-width: 992px) {
+    font-size: 2rem;
+  }
+`
+
+const PageTitle = ({ titleText }) => <StyledPageTitle>{titleText}</StyledPageTitle>
 
 const PageHeader = () => {
   return (
     <PageHeaderContainer>
-      <PageTitle title={"What's For Dinner?"}/>
+      <PageTitle titleText={"What's For Dinner?"}/>
     </PageHeaderContainer>
   )
 }
